b32ccf08d201228bd8cc05d618cd9adb27600ae1
Chris Austin <caustin@zeomega.com>
2013-12-04 13:37:24 -0600

    adding new models

--- thirdparty/admin.py
+++ thirdparty/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from thirdparty.models import ThirdPartyComponent, License
+from thirdparty.models import (
+    ThirdPartyComponent,
+    License,
+    Component,
+    Vulnerability
+)
 
 
 admin.site.register(ThirdPartyComponent)
 admin.site.register(License)
+admin.site.register(Component)
+admin.site.register(Vulnerability)
